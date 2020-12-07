e7e4e36262fe483fc3e9203416eaa27572c32e00
clebsonf <clebson.augusto@dcx.ufpb.br>
2020-12-07 06:58:29 -0300

    feat: consumer entity created

---
+++ src/main/java/com/ayty/fintech/domain/entity/Consumer.java
@@ -0,0 +1,36 @@
+package com.ayty.fintech.domain.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+
+import java.io.Serializable;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Consumer implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false,unique = true)
+    private String username;
+
+}
