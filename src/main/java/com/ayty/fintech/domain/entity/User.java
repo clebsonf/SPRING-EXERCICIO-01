5f3aee3448908ed65ee0112eca976a226b808f14
clebsonf <clebson.augusto@dcx.ufpb.br>
2020-12-07 06:29:30 -0300

    feat: user entity created

--- src/main/java/com/ayty/fintech/domain/entity/User.java
+++ src/main/java/com/ayty/fintech/domain/entity/User.java
@@ -1,2 +1,37 @@
-package com.ayty.fintech.domain.entity;public class User {
+package com.ayty.fintech.domain.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String cpf;
+
+    @Column(nullable = false, name = "full_name")
+    private String fullName;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false, name = "phone_number")
+    private String phoneNumber;
+
 }
